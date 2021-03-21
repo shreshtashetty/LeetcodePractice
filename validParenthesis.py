9682297a1299248e0a1f59cdae2ed5dea6cf53dd
Shreshta Shetty <shresta.shetty@gmail.com>
2021-03-21 21:52:01 +0530

    Solution to 20. Valid Parentheses

---
+++ validParenthesis.py
@@ -0,0 +1,37 @@
+# 20. Valid Parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = deque()
+        if len(s)<=1:
+            return False
+        
+        if s[0] == ')' or s[0] == '}' or s[0] == ']':
+            return False
+        
+        for i in range(len(s)):
+            
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.appendleft(s[i])
+                
+            if s[i] == ')':
+                if not stack: return False
+                if stack.popleft()!='(':
+                    return False
+                
+            elif s[i] == ']':
+                if not stack: return False
+                if stack.popleft()!='[':
+                    return False
+                
+            elif s[i] == '}':
+                if not stack: return False
+                if stack.popleft()!='{':
+                    return False
+                
+        if stack:
+            return False
+        
+        return True
+        
