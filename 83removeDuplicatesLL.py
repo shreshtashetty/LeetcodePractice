9bb84f2f081a26218058dca2a35787cb1780a1da
Shreshta Shetty <shreshtas94@gmail.com>
2021-11-11 15:41:21 +0530

    Solution to 83. Remove Duplicates from Sorted List

--- 83removeDuplicatesLL.py
+++ 83removeDuplicatesLL.py
@@ -4,7 +4,7 @@
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+#         self.next = next 
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
