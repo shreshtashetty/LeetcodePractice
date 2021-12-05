dd6b7ae0c6f8aa95cab585c4aac32a2afdd30908
Shreshta Shetty <shreshtas94@gmail.com>
2021-12-05 14:22:56 +0530

    Solution to 337. House Robber III

---
+++ 337_houseRobberIII.py
@@ -0,0 +1,27 @@
+# 337. House Robber III
+
+# Neetcode's Post order DFS Solution: https://www.youtube.com/watch?v=nHR8ytpzz7c
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(root):
+            if not root:
+                return [0, 0]
+            
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+            
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+            
+            return [withRoot, withoutRoot]
+        
+        return max(dfs(root))
