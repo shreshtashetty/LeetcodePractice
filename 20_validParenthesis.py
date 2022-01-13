d3c9787c5279291b9e90608688abdb2699711a81
Shreshta Shetty <shreshtas94@gmail.com>
2022-01-13 21:57:01 +0530

    Update 20_validParenthesis.py

--- 20_validParenthesis.py
+++ 20_validParenthesis.py
@@ -7,9 +7,6 @@ class Solution:
         if len(s)<=1:
             return False
         
-        if s[0] == ')' or s[0] == '}' or s[0] == ']':
-            return False
-        
         for i in range(len(s)):
             
             if s[i] == '(' or s[i] == '{' or s[i] == '[':
