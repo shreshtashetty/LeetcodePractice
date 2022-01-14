708b1523ab07f234649b47a96e8d0ecbf235668e
Shreshta Shetty <shreshtas94@gmail.com>
2022-01-14 17:28:45 +0530

    Better Solution

--- 49_groupAnagrams.py
+++ 49_groupAnagrams.py
@@ -1,4 +1,6 @@
 #49. Group Anagrams
+'''
+# A less efficient solution
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         s = []
@@ -15,4 +17,28 @@ class Solution:
                 ind = s.index(a)
                 all_[ind].append(strs[i])
         return all_
+ '''
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        l = []
+        
+        for i in range(len(strs)):
+            l.append("".join(sorted(strs[i])))
+        
+        grp_words = defaultdict(list)
+        
+        for i in range(len(strs)):
+            grp_words[l[i]].append(strs[i])
+        
+        res = []
+        
+        for k, v in grp_words.items():
+            res.append(v)
+            
+        return res
+        
         
