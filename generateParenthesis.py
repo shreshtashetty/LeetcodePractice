0f8eeb7fade0adf3edc8bffeeab3cfc311800364
Shreshta Shetty <shreshtas94@gmail.com>
2022-01-22 23:53:34 +0530

    Update generateParenthesis.py

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -1,30 +1,52 @@
 # 22. Generate Parentheses
 
+# Refer to Nick White's video.
+# Solution with Backtracking.
+
+def backtrack(res, curr, open_, close_, n):
+    if len(curr)==n*2:
+        res.append(curr)
+        return
+    
+    if open_<n:
+        backtrack(res, curr+"(", open_+1, close_, n)
+    if close_<open_:
+        backtrack(res, curr+")", open_, close_+1, n)
+
 class Solution:
-    def generateParenthesis(self, n):
-        def generate(A = []):
-            if len(A) == 2*n:
-                if valid(A):
-                    ans.append("".join(A))
-            else:
-                A.append('(')
-                generate(A)
-                A.pop()
-                A.append(')')
-                generate(A)
-                A.pop()
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        backtrack(res, "", 0, 0, n)
+        return res
+        
 
-        def valid(A):
-            bal = 0
-            for c in A:
-                if c == '(': bal += 1
-                else: bal -= 1
-                if bal < 0: return False
-            return bal == 0
 
-        ans = []
-        generate()
-        return ans
+# # Solution without backtracking
+# class Solution:
+#     def generateParenthesis(self, n):
+#         def generate(A = []):
+#             if len(A) == 2*n:
+#                 if valid(A):
+#                     ans.append("".join(A))
+#             else:
+#                 A.append('(')
+#                 generate(A)
+#                 A.pop()
+#                 A.append(')')
+#                 generate(A)
+#                 A.pop()
+
+#         def valid(A):
+#             bal = 0
+#             for c in A:
+#                 if c == '(': bal += 1
+#                 else: bal -= 1
+#                 if bal < 0: return False
+#             return bal == 0
+
+#         ans = []
+#         generate()
+#         return ans
 
 # LAME ATTEMPT(WRONG SOLUTION)    
 # class Solution:
