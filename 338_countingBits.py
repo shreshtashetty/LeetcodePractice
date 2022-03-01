6effa5c79e23b108bb584891961f26c53cd754d4
Shreshta Shetty <shreshtas94@gmail.com>
2022-03-01 12:54:21 +0530

    Solution to 338. Counting Bits

---
+++ 338_countingBits.py
@@ -0,0 +1,13 @@
+# 338. Counting Bits
+
+# https://leetcode.com/problems/counting-bits/discuss/656849/Python-Simple-Solution-with-Clear-Explanation
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        counter = [0]
+        
+        for i in range(1,n+1):
+            counter.append((counter[i>>1] + i%2))
+            
+        return counter
