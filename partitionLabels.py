c755c849574c540a43bbe6775b2e1c757d61f142
Shreshta Shetty <shreshtas94@gmail.com>
2022-03-21 13:54:35 +0530

    Add better solution to Partition Labels

--- partitionLabels.py
+++ partitionLabels.py
@@ -1,5 +1,23 @@
 # 763. Partition Labels
 
+#Better Solution
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        last = {c:i for i,c in enumerate(s)}
+        start = end = 0
+        lengths = []
+            
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+            
+            if i==end:
+                lengths.append(end-start+1)
+                start = i+1
+                
+        return lengths
+                
+'''
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         end_in = 0
@@ -15,4 +33,4 @@ class Solution:
                 start = end_in+1
                 end_in+=1
         return lengths
-                
+'''
