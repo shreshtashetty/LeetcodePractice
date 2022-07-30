1a212e629710eeaedd0820eb7cf533b9822983e1
Shreshta Shetty <shreshtas94@gmail.com>
2022-07-30 18:40:18 +0530

    Update 187_findRepeatedDnaSeqs.py

--- 187_findRepeatedDnaSeqs.py
+++ 187_findRepeatedDnaSeqs.py
@@ -3,6 +3,19 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
+        res, seen = set(), set()
+        
+        for l in range(len(s)-9):
+            
+            cur = s[l:l+10]
+            
+            if cur in seen:
+                res.add(cur)
+                
+            seen.add(cur)
+            
+        return list(res)
+        '''
         seqs = []
         
         hashset = {}
@@ -21,6 +34,7 @@ class Solution:
                 seqs.append(key)
         
         return seqs
+        '''
             
         
         
